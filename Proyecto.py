c52aa585c47edaf1a5394076e2898ed2d1fa4545
KenzoYoshihara <32112799+KenzoYoshihara@users.noreply.github.com>
2017-10-17 21:36:39 -0400

    Proyecto.py

---
+++ Proyecto.py
@@ -0,0 +1,32 @@
+# Proyecto-de-Robotica
+print("11. Factores de conversion de Caudal.")
+	a=int(input("Nro= "))
+	if a==1:
+		contador=0
+		while contador==0:
+			print("Factores de Conversion de Longitud.")
+			print("1. Factores de Conversion de Longitud en el Sistema Internacional.")
+			print("2. Factores de Conversion de Longitud en el Sistema Sexagesimal.")
+			print("3. Factores de Conversion de Longitud en el sistema Ingles.")
+			print("4. Factores de Conversion de LOngitud en el Sistema .")
+			print("5. Atras...")
+			a=int(input("Nro= ")
+			
+			if a==1:
+				print("Factores de Conversion de Longitud en el Sistema Internacional.")
+				print("Seleccione el factor de conversion que desea convertir.")
+				print("Metros.")
+				print("Centimetros.")
+				print("Milimetros.")
+				print("Micron")
+				print("Angstrom.")
+				print("Kilometros")
+				print("Milla Terrestre.")
+				print("Milla Nautica.")
+				print("Legua.")
+				print("Yardas.")
+				print("Pie.")
+				print("Pulgada.")
+				print("Año Luz.")
+				print("Parsec.")
+	
